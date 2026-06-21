f27968ae47e46dc54604c3d4899fae5b866c76f1
Abbas Al Masri <wopalncom@gmail.com>
2026-06-21 03:06:03 +0300

    feat: add Google Analytics 4 tracking (G-4HGH2F0YX3)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { cookies } from "next/headers";
+import Script from "next/script";
 import { NextIntlClientProvider } from "next-intl";
 import { PWARegister } from "@/components/PWARegister";
 import { PWAInstallPrompt } from "@/components/PWAInstallPrompt";
@@ -89,6 +90,20 @@ export default async function RootLayout({
         <PWARegister />
         <PWAInstallPrompt />
         <AppDownloadModal />
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="ga4" strategy="afterInteractive">{`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: window.location.pathname });
+            `}</Script>
+          </>
+        )}
       </body>
     </html>
   );
